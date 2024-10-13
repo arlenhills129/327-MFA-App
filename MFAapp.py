8bae86242c2721e5c09b067b128a5751f64fa7be
Arlen Hills <arlen.hills@wsu.edu>
2024-10-12 19:29:15 -0700

    Added Question Class
    Question class will be used to store questions and for relating chosen questions to users.

--- MFAapp.py
+++ MFAapp.py
@@ -1,11 +1,11 @@
 from app import create_app, db
-from app.backend.Model.models import User
+from app.backend.Model.models import User, Question
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': app.db, 'User': User}
+    return {'db': app.db, 'User': User, 'Question':Question}
 
 @app.before_request
 def initDB(*args, **kwargs):
--- app/backend/Model/models.py
+++ app/backend/Model/models.py
@@ -8,3 +8,8 @@ class User(db.Model):
     username = db.Column(db.String(60), unique=True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String(120), unique=True)
+
+class Question(db.Model):
+    __tablename__ = 'question'
+    id = db.Column(db.Integer, primary_key=True)
+    choice = db.Column(db.String(60), unique=True)
